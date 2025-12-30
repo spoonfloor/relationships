ef9a527eb6053d6665c60493eea2f00afd70eb75
edk-abstract-curves <edk-abstract-curves@users.noreply.github.com>
2025-12-30 16:44:36 +0000

    deploy: 276a2d5a768c25c9af79b75da12a669eb8bf05f3

---
+++ src/color.js
@@ -0,0 +1,66 @@
+/**
+ * @fileoverview Copyright 2025 Ed Korthof and Cristie Henry
+ */
+
+export function hexToRgb(hex) {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+}
+
+export function rgbToHex(r, g, b) {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).padStart(6, '0');
+}
+
+export function rgbToLab(rgb) {
+  let r = rgb.r / 255,
+    g = rgb.g / 255,
+    b = rgb.b / 255,
+    x,
+    y,
+    z;
+
+  r = r > 0.04045 ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+  g = g > 0.04045 ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+  b = b > 0.04045 ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+
+  x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+  y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.0;
+  z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+
+  x = x > 0.008856 ? Math.pow(x, 1 / 3) : 7.787 * x + 16 / 116;
+  y = y > 0.008856 ? Math.pow(y, 1 / 3) : 7.787 * y + 16 / 116;
+  z = z > 0.008856 ? Math.pow(z, 1 / 3) : 7.787 * z + 16 / 116;
+
+  return {
+    l: 116 * y - 16,
+    a: 500 * (x - y),
+    b: 200 * (y - z),
+  };
+}
+
+export function deltaE(labA, labB) {
+  const deltaL = labA.l - labB.l;
+  const deltaA = labA.a - labB.a;
+  const deltaB = labA.b - labB.b;
+  const c1 = Math.sqrt(labA.a * labA.a + labA.b * labA.b);
+  const c2 = Math.sqrt(labB.a * labB.a + labB.b * labB.b);
+  const deltaC = c1 - c2;
+  let deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC;
+  deltaH = deltaH < 0 ? 0 : Math.sqrt(deltaH);
+  const sc = 1.0 + 0.045 * c1;
+  const sh = 1.0 + 0.015 * c1;
+  const deltaLKlsl = deltaL / 1.0;
+  const deltaCkcsc = deltaC / sc;
+  const deltaHkhsh = deltaH / sh;
+  const i =
+    deltaLKlsl * deltaLKlsl +
+    deltaCkcsc * deltaCkcsc +
+    deltaHkhsh * deltaHkhsh;
+  return i < 0 ? 0 : Math.sqrt(i);
+}
